4895fd2df67c3c67e31275eb706483dabe4b1f82
Dan Setterquist <dansetterquist@gmail.com>
2012-05-06 16:02:49 +0200

    Make Moon an Entity.

--- src/ddmp/projecttetra/entity/Moon.java
+++ src/ddmp/projecttetra/entity/Moon.java
@@ -1,11 +1,9 @@
-package ddmp.projecttetra;
+package ddmp.projecttetra.entity;
 
 import java.util.List;
 
 import org.andengine.engine.Engine;
-import org.andengine.entity.shape.IShape;
 import org.andengine.entity.sprite.Sprite;
-import org.andengine.extension.physics.box2d.PhysicsConnector;
 import org.andengine.extension.physics.box2d.PhysicsFactory;
 import org.andengine.extension.physics.box2d.PhysicsWorld;
 import org.andengine.opengl.texture.ITexture;
@@ -17,48 +15,39 @@ import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
 import com.badlogic.gdx.physics.box2d.Contact;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
 
-import ddmp.projecttetra.entity.MoonPiece;
+import ddmp.projecttetra.Comet;
+import ddmp.projecttetra.RegionManager;
+import ddmp.projecttetra.TetraActivity;
 
 /**
  * A moon in the game.
  */
-public class Moon {
+public class Moon extends Entity {
 	
 	private static final FixtureDef MOON_FIXTURE_DEF = PhysicsFactory.createFixtureDef(1, 0.5f, 0.5f, true);
 	private static final float MOON_MIN_SIZE = 0.08f; //In percent of camera height
 	private static final float MOON_MAX_SIZE = 0.12f;	//In percent of camera height
 	
-	private Engine engine;
-	private PhysicsWorld physicsWorld;
-	private PhysicsConnector con;
-	private Body body;
-	private Sprite shape;
-	private boolean dead;
-
-	public Moon(float x, float y, Engine engine, PhysicsWorld physicsWorld) {
-		this.engine = engine;
-		this.physicsWorld = physicsWorld;
-		this.dead = false;
-		
+	public static Moon createMoon(Engine engine, PhysicsWorld physicsWorld, float x, float y) {
 		float scale = MOON_MIN_SIZE + (MOON_MAX_SIZE - MOON_MIN_SIZE) * (float) Math.random();
 		float size = scale * TetraActivity.CAMERA_HEIGHT;
-		shape = new Sprite(x, y, size, size, RegionManager.getInstance().get(RegionManager.Region.MOON), 
-				engine.getVertexBufferObjectManager());
-		body = PhysicsFactory.createCircleBody(physicsWorld, shape, 
-				BodyType.StaticBody, MOON_FIXTURE_DEF);
-		body.getFixtureList().get(0).setUserData(this); /* A bit hacky. */
-		this.con = new PhysicsConnector(shape, body, true, true);
+		Sprite sprite = new Sprite(x, y, size, size, RegionManager.getInstance().get(
+				RegionManager.Region.MOON), engine.getVertexBufferObjectManager());
+		Body body = PhysicsFactory.createCircleBody(physicsWorld, sprite, BodyType.StaticBody,
+				MOON_FIXTURE_DEF);
+		return new Moon(engine, physicsWorld, sprite, body);
 	}
 	
-	public void update() {
+	private Moon(Engine engine, PhysicsWorld physicsWorld, Sprite sprite, Body body) {
+		super(engine, physicsWorld, sprite, body);
+	}
+	
+	@Override
+	public void onUpdate(float pSecondsElapsed) {
 		checkForCollisionWithCommet();
 	}
 	
 	private void checkForCollisionWithCommet() {
-		if(dead) {
-			return;
-		}
-		
 		List<Contact> contacts = physicsWorld.getContactList();
 		Contact contact = null;
 		int size = contacts.size();
@@ -71,13 +60,13 @@ public class Moon {
 						(aData instanceof Comet || bData instanceof Comet)) {
 					/* This has collided with comet, break apart. */
 					breakApart();
+					destroySelf();
 				}
 			}
 		}
 	}
 	
 	private void breakApart() {
-		dead = true;
 		ITextureRegion moonTextureRegion = RegionManager.getInstance().get(RegionManager.Region.MOON);
 		ITexture texture = moonTextureRegion.getTexture();
 		int tX = (int) moonTextureRegion.getTextureX();
@@ -89,29 +78,19 @@ public class Moon {
 		ITextureRegion reg3 = TextureRegionFactory.extractFromTexture(texture, tX, tY+tH/2, tW/2, tH/2);
 		ITextureRegion reg4 = TextureRegionFactory.extractFromTexture(texture, tX+tW/2, tY+tH/2, tW/2, tH/2);
 		
-		float sX = shape.getX();
-		float sY = shape.getY();
-		float size = shape.getWidth() / 2;
+		Sprite sprite = (Sprite) bodySpriteConnector.getShape();
+		float sX = sprite.getX();
+		float sY = sprite.getY();
+		float size = sprite.getWidth() / 2;
 		MoonPiece.createMoonPiece(engine, physicsWorld, sX, sY, size, reg1).registerSelf();
 		MoonPiece.createMoonPiece(engine, physicsWorld, sX+size, sY, size, reg2).registerSelf();
 		MoonPiece.createMoonPiece(engine, physicsWorld, sX, sY+size, size, reg3).registerSelf();
 		MoonPiece.createMoonPiece(engine, physicsWorld, sX+size, sY+size, size, reg4).registerSelf();
 	}
-	
-	public Body getBody() {
-		return con.getBody();
-	}
-	
-	public IShape getShape() {
-		return con.getShape();
-	}
-	
-	public PhysicsConnector getPhysicsConnector() {
-		return con;
-	}
-	
-	public boolean isDead() {
-		return dead;
+
+	@Override
+	public void reset() {
+		
 	}
 
 }
